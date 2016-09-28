369213e6d8b9231ee501835494384635b2de9646
Quentin Bergougnoux <quentin.bergougnoux@gmail.com>
2016-09-28 17:32:03 +0200

    Replace Compiler by Interpreter

--- iti.c
+++ iti.c
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        printf("The all-mighty IfTrue Compiler\n");
+        printf("The all-mighty IfTrue Interpreter\n");
         printf("Usage: %s [source]\n", argv[0]);
         exit(-1);
     }
